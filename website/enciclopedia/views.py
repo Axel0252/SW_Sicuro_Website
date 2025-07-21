58bbdc22912b2fc49591bb9ac90785c82c94d111
Lara Maggiulli <lara.maggiulli001@studenti.uniparthenope.it>
2025-07-21 16:18:40 +0200

    sistemazione enciclopedia

--- website/enciclopedia/views.py
+++ website/enciclopedia/views.py
@@ -17,6 +17,10 @@ def enciclopedia_indice(request):
 def enciclopedia_attacchi(request, attacco_id):
     attacco = get_object_or_404(Attacco, id=attacco_id)
 
+    # Correggi i \n testuali in veri newline
+    attacco.descrizione = attacco.descrizione.replace('\\n', '\n')
+    attacco.contromisure = attacco.contromisure.replace('\\n', '\n')
+
     if request.user.is_authenticated:
         ConsultazioneAttacco.objects.create(
             attacco=attacco,
