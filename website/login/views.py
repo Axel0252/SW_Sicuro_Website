88fe1d07bbd5f139ede74e214bbb184850bdad7e
Axel0252 <tafuro561@gmail.com>
2025-07-21 12:24:53 +0200

    implementing login

---
+++ website/login/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from enciclopedia.models import *
+import hashlib
+from django.core.exceptions import ObjectDoesNotExist # Gestisce sia DoesNotExist che MultipleObjectsReturned
+
+def index(request):
+    return render(request, 'loginIndex.html')
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get('password')
+        hashed_password = hashlib.md5(password.encode()).hexdigest()
+
+        try:
+            user_data = Utente.objects.filter(email=email, password=password).get()
+        except ObjectDoesNotExist:
+            render(request, 'loginIndex.html', {'error_message' : "Email e/o password non validi"})
+        
+        
