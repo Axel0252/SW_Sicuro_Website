468b86be653934b40bd95c998e6df302d068a702
Lara Maggiulli <lara.maggiulli001@studenti.uniparthenope.it>
2025-07-21 17:36:35 +0200

    sistemata enciclopedia_attacchi.html e enciclopedia_indice.html

--- website/enciclopedia/views.py
+++ website/enciclopedia/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.utils import timezone
+import re
+from django.utils.html import escape, mark_safe
 from django.shortcuts import render, redirect, get_object_or_404
 from enciclopedia.models import EnciclopediaAttacchi, ConsultazioneAttacco, Attacco
+
 
 
 # Create your views here.
@@ -14,12 +17,35 @@ def enciclopedia_indice(request):
     categorie = EnciclopediaAttacchi.objects.all()
     return render(request, 'enciclopedia_indice.html', {'categorie': categorie})
 
+import re
+from django.utils.html import escape, mark_safe
+
 def enciclopedia_attacchi(request, attacco_id):
     attacco = get_object_or_404(Attacco, id=attacco_id)
 
-    # Correggi i \n testuali in veri newline
-    attacco.descrizione = attacco.descrizione.replace('\\n', '\n')
-    attacco.contromisure = attacco.contromisure.replace('\\n', '\n')
+    def formatta_testo(testo):
+        # Pulisce newline e backslash
+        testo = testo.replace('\\n', '\n').replace('\\', '')
+
+        # Escape del testo per evitare XSS
+        from django.utils.html import escape, mark_safe
+        testo = escape(testo)
+
+        # Evidenzia titoli (senza lookbehind)
+        titoli = [
+            "Modalità di esecuzione:",
+            "Possibili conseguenze:",
+            "Consigli pratici per la prevenzione:"
+        ]
+
+        for titolo in titoli:
+            # Sostituisce il titolo con la versione in <strong>
+            testo = testo.replace(titolo, f"<strong>{titolo}</strong>")
+
+        return mark_safe(testo)
+
+    attacco.descrizione = formatta_testo(attacco.descrizione)
+    attacco.contromisure = formatta_testo(attacco.contromisure)
 
     if request.user.is_authenticated:
         ConsultazioneAttacco.objects.create(
