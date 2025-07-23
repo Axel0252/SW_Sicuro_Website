c31899512069db7e65f4eeaf04c2ab520c563414
Axel0252 <tafuro561@gmail.com>
2025-07-23 15:55:34 +0200

    Aggiornato form registrazione azienda e urls

--- website/login/urls.py
+++ website/login/urls.py
@@ -6,8 +6,6 @@ from .views import *
 urlpatterns = [
     path('', index, name="index"),
     path('checkLogin', checkLogin, name="login"),
-    path('scelta_privato', scelta_privato, name="scelta_privato"),
-    path('scelta_azienda', scelta_azienda, name="scelta_azienda"),
     path('registrazione_azienda', registrazione_azienda),
     path('registrazione_privato', registrazione_privato),
 ]
